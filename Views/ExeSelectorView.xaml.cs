4054deee3efd539346d05e7be498c9b90999396e
Oleg Savelos <olegsavelos@netprojects.onmicrosoft.com>
2020-09-12 14:19:51 +0300

    Exe selector addition

---
+++ Views/ExeSelectorView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Client.Views;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+
+namespace MaterialDesignSkin.Views
+{
+    /// <summary>
+    /// Interaction logic for ExeSelectorView.xaml
+    /// </summary>
+    [Export(typeof(IExeSelectorView))]
+    public partial class ExeSelectorView : UserControl, IExeSelectorView
+    {
+        public ExeSelectorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
