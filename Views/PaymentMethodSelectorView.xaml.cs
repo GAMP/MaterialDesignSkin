642daecdffb91939cd06e95dda762e5258cd03a5
klapsis-netprojects <klapsis@netprojects.gr>
2021-03-22 13:15:10 +0200

    Task 36: Payment method selector.

---
+++ Views/PaymentMethodSelectorView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Client.Views;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+
+namespace MaterialDesignSkin.Views
+{
+    /// <summary>
+    /// Interaction logic for PaymentMethodSelectorView.xaml
+    /// </summary>
+    [Export(typeof(IPaymentMethodSelectorView))]
+    public partial class PaymentMethodSelectorView : UserControl, IPaymentMethodSelectorView
+    {
+        public PaymentMethodSelectorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
