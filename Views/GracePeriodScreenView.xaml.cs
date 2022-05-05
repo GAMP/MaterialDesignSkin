5566ece6ba29aeec80f2b24bef548afc99f93a65
klapsis-netprojects <klapsis@netprojects.gr>
2022-05-05 11:50:47 +0300

    Grace period client UI.

---
+++ Views/GracePeriodScreenView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Client.Views;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+
+namespace MaterialDesignSkin.Views
+{
+    /// <summary>
+    /// Interaction logic for GracePeriodScreenView.xaml
+    /// </summary>
+    public partial class GracePeriodScreenView : UserControl
+    {
+        public GracePeriodScreenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
