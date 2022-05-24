6d985efc1b4744542f9cfe23cff80595dd1799e8
klapsis-netprojects <klapsis@netprojects.gr>
2022-05-24 13:51:21 +0300

    Add user agreement after login.

---
+++ Views/UserAgreementsControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace MaterialDesignSkin.Views
+{
+    /// <summary>
+    /// Interaction logic for UserAgreementsControl.xaml
+    /// </summary>
+    public partial class UserAgreementsControl : UserControl
+    {
+        public UserAgreementsControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
